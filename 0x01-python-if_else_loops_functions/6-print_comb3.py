f4229369c45341f510fb3b14fbb6892ba22914e3
alsanhory <elrayah.alsasnhory@gmail.com>
2023-11-29 05:43:00 -0800

    print combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for num1 in range(10):
     for num2 in range(num1 + 1, 10):
         if num1 < 8 or num2 < 9:
-            ed=", "
+            ed = ", "
         else:
-            ed="\n"
-        print("{:d}{:d}".format(num1,num2), end=ed)
+            ed = "\n"
+        print("{:d}{:d}".format(num1, num2), end=ed)
