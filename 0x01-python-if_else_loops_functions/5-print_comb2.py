073c4c4149b14bae55454ecad13f5bc5da2d1e64
alsanhory <elrayah.alsasnhory@gmail.com>
2023-11-28 23:56:39 -0800

    print combo

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(100):
-    thened= ", " if i < 99 else "\n"
-    print("{:02d}".format(i), end = thened)
+    thened = ", " if i < 99 else "\n"
+    print("{:02d}".format(i), end=thened)
