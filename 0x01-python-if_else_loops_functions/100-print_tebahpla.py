4a52707bac30547c8d342e79f8be9d5b87d1460a
alsanhory <elrayah.alsasnhory@gmail.com>
2023-11-29 17:36:12 -0800

    char and removechar

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for char in range(122, 96, -1):
+    if char % 2 == 0:
+        char = char
+    else:
+        char = char -32
+    print("{:c}".format(char), end="")
+print()
+
