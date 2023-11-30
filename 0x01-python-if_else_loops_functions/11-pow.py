08e761e43860620407e570853f9f783f0675523d
alsanhory <elrayah.alsasnhory@gmail.com>
2023-11-29 17:25:28 -0800

    power

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def pow(a, b):
     if b < 0:
-        a = 1 / a
-        b = -b
-
-    res = 1
-    for i in range(b):
-        res *= a
-
-    return round(res, 2)
+        return 1 / (a ** abs(b))
+    else:
+        return a ** b
