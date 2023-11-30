9dec1a4699c271b69f21e7bed504d0ac49e3bd5d
alsanhory <elrayah.alsasnhory@gmail.com>
2023-11-29 17:44:02 -0800

    committing

---
+++ 0x00-python-hello_world/testing.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(1, 10):
+    if i <= 5:
+        print("hhhaa")
+    elif i >= 6 and i <= 8:
+        print ("WHERE ARE ALL THE BRACKETS???")
+    else:
+        print ("HOW DO YOU PEOPLE READ THIS SYNTAX")
